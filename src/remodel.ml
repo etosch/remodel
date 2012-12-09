3f894ed34393dcad7436d7a5b59f9519b2fec385
Emma Tosch <etosch@gmail.com>
2012-12-09 10:24:04 -0500

    scrapped actual functional graph representation. just doing an a-list. set up for executing serially

--- src/remodel.ml
+++ src/remodel.ml
@@ -1,16 +1,11 @@
+(* Future Improvements? *)
+(* 1. When handling multiple dependencies/targets, always sort - i.e. don't just use them in the same order as the REMODELFILE? *)
+
 try 
-	Unix.mkdir ".remodel" 0o700 
+	Unix.mkdir ".remodel" 0o755 ;
+	Unix.close (Unix.openfile ".remodel/history" [Unix.O_CREAT] 0o755) ; 
 with Unix.Unix_error(_,_,_) -> ();;
-(*
- Remodel: Make Done Right
- Due Date: December 12 2012
 
- For this individual project, you will write a replacement program for "make" called "remodel." Make takes a series of dependencies and builds targets by executing commands. Unfortunately, its use of UNIX timestamps as a happens-before relation means that it can fail to build properly. This is especially true during parallel builds.
-
- Your program, "remodel", will also take a series of dependencies and targets, etc. (grammar below), but unlike make, it will use MD5 hashes to detect new content and provide, together with the dependency chain, a happens-before relation (md5diff(oldA,newA) => everything that depends on A must be rebuilt). It must also execute all independent (non-conflicting) commands in parallel, using threads or processes.
-
- remodel uses a different grammar than make. Dependencies can appear in any order. If you execute "remodel" with no arguments, it should start with the pseudo-target "DEFAULT". Otherwise, the root is the argument to "remodel", as in "remodel foo.o".
-*)
 type command = Command of string;;
 type filename = Filename of string;;
 type target = Target of filename list;;
@@ -18,81 +13,79 @@ type dependency = Dependency of filename list;;
 type production = Production of target * dependency * command;;
 type program = Program of production list;;
 
-(* will need to write a parser to produce this. for now write these files like so - i.e. put off parsing until the end *)
-(*
- Here's an example that builds the program "baz" from two source files, "foo.cpp" and "bar.cpp".
-
- DEFAULT <- baz
- baz <- foo.o, bar.o: "g++ foo.o bar.o -o baz"
- foo.o <- foo.cpp : "g++ -c foo.cpp -o foo.o"
- bar.o <- bar.cpp: "g++ -c bar.cpp -o bar.o"
-*)
-let example1 = Program([Production(Target([Filename("DEFAULT")]), Dependency([Filename("baz")]), Command("")) ;
-						Production(Target([Filename("baz")]), Dependency([Filename("foo.o") ; Filename("bar.o")]), Command("g++ foo.o bar.o -o baz")) ;
-						Production(Target([Filename("foo.o")]), Dependency([Filename("foo.cpp")]), Command("g++ -c foo.cpp -o foo.o")) ;
-						Production(Target([Filename("bar.o")]), Dependency([Filename("bar.cpp")]), Command("g++ -c bar.cpp -o bar.o"))
-						]);;
-(*
- You should store the dependencies on disk in a special directory called ".remodel/", so that "remodel" will not re-execute any commands unless a dependency has been violated.
-*)
-(* The following implementation for executing a system command and returning the values from stdout is from rosettacode.org. *)
+type md5 = MD5 of string;;
+type node = Empty | Node of filename * md5 * command * filename list;;
+type graph = Uninstantiated | Graph of md5 * node list;;
 
 exception MoreThanOneTargetException of string;;
 exception NodeCreationException of string;;
-type md5 = MD5 of string;;
-type node = Empty | Node of filename * md5 * command * node list;;
-type graph = Uninstantiated | Graph of md5 * node list;;
 
-let get_md5 (Filename(f)) = MD5(Digest.to_hex (Digest.file f))
+let make_md5 (Filename(f)) = if f = "DEFAULT" then MD5("") else MD5(Digest.to_hex (Digest.file f));;
+let get_md5 (MD5(s)) = s;;
 
-let make_dependency_graph (prod_list : production list) =
+let make_serial_commands (Program(prod_list)) (target : filename) =
 	let targets = List.flatten (List.map (fun (Production(Target(file_list),_,_)) -> file_list) prod_list)
-in  let rec unique_files = function
-	| (d, []) -> (d, [])
-	| (d, h::t) -> unique_files (h::d, (List.filter (fun f -> f <> h) t))
-in let rec all_files = function
-	| [] -> []
-	| Production(Target(l1),Dependency(l2),_)::t -> l1 @ l2 @ all_files(t)
-in let find_target (f : filename) = 
+in	let rec make_node_list = function
+ 	 | [] -> []	
+ 	 | Production(Target([]), _, _)::t -> make_node_list t
+ 	 | Production(Target(h::t1), Dependency(dep_list), cmd)::t2 -> 
+		Node(h, make_md5 h, cmd, dep_list)::make_node_list(Production(Target(t1), Dependency(dep_list), cmd)::t2)
+in  let is_valid_target (f : filename) = 
 	match List.filter (fun ff -> ff = f) targets with 
-	| [] -> None (* files that are dependencies but not targets will be picked up *)
-	| h::[] -> Some(List.hd targets) 
-	| h::t -> let Filename(fname) = f in
-		raise (MoreThanOneTargetException fname)
+	 | [] -> false
+	 | h::[] -> true
+	 | h::t -> let Filename(fname) = f in raise (MoreThanOneTargetException fname)
+in 	let rec get_from_node_list (f : filename) (n : node list)	=
+	match n with
+	 | [] -> Empty
+	 | (Node(ff,_,_,_) as h)::t when ff = f -> h
+	 | _::t -> get_from_node_list f t
+in  let rec get_commands_for_target (f : filename) (n : node list) =
+	match get_from_node_list f n with
+	| Empty -> []
+	| Node(_,_,cmd,[]) -> [cmd] 
+	| Node(_,_,cmd,dep_list) -> (List.flatten (List.map (fun a -> get_commands_for_target a n) dep_list)) @ [cmd]
+in get_commands_for_target target (make_node_list prod_list);;
+
+(* let exec_serial_commands (cmds : command list) = *)
+
+let record_dependencies (Program(prod_list)) = 
+	let rec helper = function
+	| [] -> ""
+	| Production(Target([]), _, _)::t -> helper(t)
+	| Production(Target((Filename(f) as h)::t1), Dependency(l), cmd)::t2 -> 
+		let concat_hashes a b = a^" "^b
+	in 	let make_md5s_for_deps = List.map (fun f -> get_md5 (make_md5 f)) l
+	in 	let prefix = if f = "DEFAULT" then f else f^" "^(get_md5 (make_md5 h))
+	in 	(List.fold_left concat_hashes prefix make_md5s_for_deps)^"\n"^helper(Production(Target(t1), Dependency(l), cmd)::t2)
+in 	let deps = helper prod_list 
+and history = open_out ".remodel/history"
+in 	output_string history deps; close_out history;; 
 
 
 exception MultipleInitialTargetException of string;;
 
-let filename_string (Filename(fname)) = fname;;
 
+(* let test_suite (arg : unit) = *)
+	let example1 = Program([Production(Target([Filename("DEFAULT")]), Dependency([Filename("baz")]), Command("")) ;
+							Production(Target([Filename("baz")]), Dependency([Filename("foo.o") ; Filename("bar.o")]), Command("g++ foo.o bar.o -o baz")) ;
+							Production(Target([Filename("foo.o")]), Dependency([Filename("foo.cpp")]), Command("g++ -c foo.cpp -o foo.o")) ;
+							Production(Target([Filename("bar.o")]), Dependency([Filename("bar.cpp")]), Command("g++ -c bar.cpp -o bar.o"))
+							]) ;;
+(* in record_dependencies example1 *)
+(* 
+	let Program(prod_list) = example1
+	in let nodes = make_node_list prod_list
+	in get_commands_for_target (Filename("DEFAULT")) nodes ;;
+*)
+
+
+(* program entry *)
 let args = Sys.argv 
 in let target = match Array.length args with
 	| 1 -> Filename("DEFAULT") 
 	| 2 -> Filename(args.(1))
 	| _ -> 	raise (MultipleInitialTargetException "Multiple initial target values are not currently supported")
-in let make_md5s line =
-	let split_string s = 
-		let space_index = String.index s ' ' in
-		let first_half = String.sub s 0 space_index
-		and second_half = String.sub s (space_index + 1) (String.length s - space_index - 1) in
-		(first_half, second_half)
-	let helper s = match String.index 
+in true;
 
-	(* first thing is the target name, so lob this off *)
-in let get_old_dependencies (Filename(f)) =
-	try 
-		let open_file = open_in ".remodel/history" 
-		and line = ref("")
-		and switch = ref(true) in
-		(try 
-			while !switch
-			do
-				line := (input_line open_file);
-				if String.sub !line 0 (String.index !line ' ') = f 
-				then switch := false
-				else ()
-			done;
-		with End_of_file -> line := "");
-		close_in open_file; make_md5s !line
-	with Sys_error(msg) -> (Printf.printf "%s" ("Warning : "^msg^"\n"	)); ""
-in Printf.printf "%s" (get_old_dependencies target);;			
+ 
---
+++ src/scratch.ml
@@ -0,0 +1,101 @@
+type 'a bin_tree = Empty | Node of 'a bin_tree * 'a * 'a bin_tree;;
+
+let myTree = Node(Empty, 0, Empty);;
+
+(* The following does not compile; I get a type error, even with annotations -> was applying the function twoce!
+let rec in_order (t : 'a bin_tree) = function
+  Empty -> ([] : 'a list) 
+| Node((lb : 'a bin_tree), (v : 'a), (rb : 'a bin_tree)) -> (in_order lb) @ [v] @ (in_order rb);; *)
+
+let rec list_of_tree = function
+	Empty -> [] |
+	Node(lb, v, rb) -> (in_order lb) @ (v :: (in_order rb));;
+
+let rec insert x = function 
+	Empty -> Node(Empty, x, Empty) |
+	Node(lb, v, rb) -> if (x < v) then Node(insert x lb, v, rb) else Node(lb, v, insert x rb);;
+
+let rec tree_of_list = function
+	[] -> Empty |
+	h :: t -> insert h (tree_of_list t);;
+
+let sort thing = list_of_tree tree_of_list thing;;
+
+
+let syscall (cmd : string) : string =
+	let ic, oc = Unix.open_process cmd in
+	let buf = Buffer.create 16 in
+	(try 
+		while true do
+			Buffer.add_channel buf ic 1
+		done
+	with End_of_file -> ());
+	let _ = Unix.close_process (ic, oc) in
+	(Buffer.contents buf);;
+
+let here = Unix.getcwd ();;
+
+
+(* Need to transform a parsed example into a dependency graph *)
+(* A graph node has associated with it the file name, the current md5 sum, and the md5 sum stored in .remodel/dependencies.ml *)
+type md5 = MD5 of string;;
+type node = Node of filename * md5 * dependency;;
+type graph = Graph of node list;;
+
+
+in  let create_node (f : filename) = 
+	match find_target(f) with
+	| None -> let Filename(fname) = f in
+		raise (NodeCreationException fname)
+	| Some(Production(_, Dependency(filename_list), cmd)) = Node(f, get_md5 f, cmd, filename_list)
+and let insert_node (n : Node) (g : Graph) =
+		let (Node(_,_,_,filename_list), Graph(node_list)) = (n , g) in
+		match filename_list with
+		| [] -> Graph(n::node_list)
+
+
+(*	let helper s = match String.index *)
+in let get_old_dependencies (Filename(f)) =
+	try 
+		let open_file = open_in ".remodel/history" 
+		and line = ref("")
+		and switch = ref(true) in
+		(try 
+			while !switch
+			do
+				line := (input_line open_file);
+				if String.sub !line 0 (String.index !line ' ') = f 
+				then switch := false
+				else ()
+			done;
+		with End_of_file -> line := "");
+		close_in open_file; "" (* do something with !line *)
+	with Sys_error(msg) -> (Printf.printf "%s" ("Warning : "^msg^"\n")); ""
+	(* Given a parsed representation of a REMODELFILE, spit out the md5 hashes and dependencies into .remodel/history *)
+
+	in let make_md5s line =
+	let split_string s = 
+		let space_index = String.index s ' ' in
+		let first_half = String.sub s 0 space_index
+		and second_half = String.sub s (space_index + 1) (String.length s - space_index - 1) in
+		(first_half, second_half)
+
+
+		
+
+in let rec find_node_by_filename (f : filename) (n : node_list) =
+	if List.length n = 0
+	then Independent(f)
+	else
+		let candidates = List.filter (fun nn -> match nn with Independent(ff) | Dependent(ff,_,_,_) -> ff = f) n in
+		match List.length candidates with
+		| 0 -> find_node_by_filename f (List.fold_left List.concat [] (List.map (fun nn -> match nn with Independent(_) -> [] | Dependent(_,_,_,l) -> l) n))
+		| 1 -> List.hd candidates
+		| _ -> let Filename(fname) = f in raise (MoreThanOneTargetException "There is more than one node of "^f)
+in let add_node (f : filename) (dep_list : dependency) (cmd : command) (g : Graph) = 
+	let Graph(_, n) = g 
+	in 
+		let dependency_node_list = List.map (fun f -> find_node_by_filename f n) dep_list
+		and 
+
+
