d0fe1324412059203b2e91b42b6aa6a713c628f8
Emma Tosch <etosch@gmail.com>
2012-12-09 13:43:17 -0500

    added code to create the cpp files for the test

--- make_examples.sh
+++ make_examples.sh
@@ -1,2 +1,5 @@
 # make examples
 touch baz foo.o bar.o foo.cpp bar.cpp
+cd tests
+printf "#include <iostream>\nvoid foo() { std::cout << \"Hello, foo!\"; }" > foo.cpp
+printf "#include <iostream>\nextern int foo (); void bar() { std::cout << \"Hello, bar!\"; } int main() { foo(); bar(); return 0; }" > bar.cpp
